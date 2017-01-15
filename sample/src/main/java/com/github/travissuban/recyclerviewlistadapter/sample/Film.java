6098fd77bbe5a6a67a27cdac4ec47c1fcf6a630a
travis <travis.suban@gmail.com>
2017-01-16 00:38:35 +0700

    add sample stub

---
+++ sample/src/main/java/com/github/travissuban/recyclerviewlistadapter/sample/Film.java
@@ -0,0 +1,9 @@
+package com.github.travissuban.recyclerviewlistadapter.sample;
+
+public class Film {
+    public long episodeId;
+    public String title;
+    public String releaseDate;
+    public String director;
+    public String openingCrawl;
+}
--- sample/src/main/java/com/github/travissuban/recyclerviewlistadapter/sample/MainActivity.java
+++ sample/src/main/java/com/github/travissuban/recyclerviewlistadapter/sample/MainActivity.java
@@ -1,15 +1,30 @@
 package com.github.travissuban.recyclerviewlistadapter.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.github.travissuban.recyclerviewlistadapter.R;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FilmAdapter filmAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.rvFilms);
+
+        setTitle("All Star Wars Films");
+
+        filmAdapter = new FilmAdapter(null, new FilmAdapter.OnFilmClickListener() {
+            @Override
+            public void onFilmClick(Film film) {
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(filmAdapter);
+
     }
 }
